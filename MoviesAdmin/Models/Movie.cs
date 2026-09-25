a40d5c3399ba35a16b799897b127ddab951d82bb
Presley99999 <w0520334@nscc.ca>
2026-09-24 21:14:09 -0300

    Added "Movie" class

---
+++ MoviesAdmin/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Synopsis { get; set; } = string.Empty;
+
+        public string Genre {  get; set; } = string.Empty;
+
+        public string Rating {  get; set; } = string.Empty; //Age rating
+
+        public int Runtime { get; set; } //Probably in minutes 
+
+        public int ReleaseDate {  get; set; } 
+    }
+}
